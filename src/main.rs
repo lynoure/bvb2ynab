a63e731ba897f83bb240a3052cf4141d89190f8c
Lynoure Braakman <lynoure@gmail.com>
2020-06-25 21:51:02 +0200

    Taking the arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct ConversionCLI {
+    #[structopt(parse(from_os_str))]
+    infile: std::path::PathBuf,
+    #[structopt(parse(from_os_str))]
+    outfile: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("CSV converter, from Bremische Volksbank format to YNAB import format");
+    let args = ConversionCLI::from_args();
+    println!("infile: {:?}", args.infile);
+    println!("outfile: {:?}", args.outfile);
 }
