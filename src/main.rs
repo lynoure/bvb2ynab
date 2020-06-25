0128e6f213897e9289fd23eeb824136815cb3d3f
Lynoure Braakman <lynoure@gmail.com>
2020-06-25 23:03:53 +0200

    Opening and reading the file
    This doesn't work with the actual CSV file yet as BVB uses ISO-8859-15,
    and Rust takes strings in UTF-8

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,18 @@ struct ConversionCLI {
 }
 
 fn main() {
+    //TODO better place for the help txt
     println!("CSV converter, from Bremische Volksbank format to YNAB import format");
     let args = ConversionCLI::from_args();
     println!("infile: {:?}", args.infile);
     println!("outfile: {:?}", args.outfile);
+    
+    //TODO better error handling, consider BufReader
+    let content = std::fs::read_to_string(&args.infile).expect("could not read file");
+    
+    //TODO extend to do actual conversion
+    //TODO handle or convert ISO-8859-15
+    for line in content.lines() {
+        println!("{}", line);
+    }
 }
