d77886933d6cdc98685de716a1dc3e4bba6d69ef
Lynoure Braakman <lynoure@gmail.com>
2020-06-28 22:03:48 +0200

    Removed output file and chatty messages
    As the output can be redirected with shell anyway, removed it.
    As when the output is redirected, it should not have any noise,
    removed description messaging from the beginning.

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,32 @@ struct ConversionError(String);
 struct ConversionCLI {
     #[structopt(parse(from_os_str))]
     infile: std::path::PathBuf,
-    #[structopt(parse(from_os_str))]
-    outfile: std::path::PathBuf,
+}
+
+fn format(content: String) {
+    for line in content.lines() {
+        println!("{}", line);
+    }
 }
 
 fn main() -> Result<(), ConversionError> {
-    //TODO better place for the help txt
-    println!("CSV converter, from Bremische Volksbank format to YNAB import format");
+    //TODO Help txt with 'cli'
     let args = ConversionCLI::from_args();
     let infilename = args.infile.to_string_lossy();
-    println!("outfile: {:?}", args.outfile);
     
     //TODO Consider BufReader, though month or two of personal finance fits into memory fine
     let result = std::fs::read_to_string(&args.infile);
     
     let content = match result {
         Ok(content) => { content },
-        // TODO make even prettier
+        // TODO make even prettier & return unique error codes
         Err(err) => { return Err(ConversionError(format!("Error reading {}: {}",
                                                          infilename, err))); }
     };
 
     //TODO handle or convert ISO-8859-15, instead of leaving it to the user
-    for line in content.lines() {
-        println!("{}", line);
-    }
+    
+    format(content);
+
     Ok(())
 }
