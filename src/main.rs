b8219a5b3f4c975cb16d2212b2fd6aa54c510149
Lynoure Braakman <lynoure@gmail.com>
2020-06-29 23:34:17 +0200

    Joins multiline transactions & cleans statements
    Joins transactions with multiple lines (due to the bank splitting
    memos) into one CSV line for further processing.
    
    Throws away beginning and end balance lines from the end of the
    normal list of transactions
    
    Prepared test data, no test yet.

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,20 @@ fn format(content: String) {
     // Could also just skip the first 13 lines, but that would be fragile
     let beginning = content.find("\"Buchungstag\";\"Valuta\"").unwrap();
     let transactions = content.get(beginning..).unwrap();
+    let mut complete = "".to_string();
     for line in transactions.lines().skip(1) {
-        println!("{}", line);
+        complete = complete + line;
+        // "S" or "H" is the last field on a complete transaction
+        if complete.rfind("\"S\"") == None && complete.rfind("\"H\"") == None {
+            continue;
+        } else {
+            if complete.contains(";;;;;;;;;") {
+                break;
+            };
+            // TODO process the complete line to YNAB format
+            println!("{}", complete);
+            complete = "".to_string();
+        }
     }
 }
 
@@ -24,12 +36,22 @@ fn main() -> Result<(), ExitFailure> {
     let args = ConversionCLI::from_args();
     let infilename = args.infile.as_path().display().to_string();
 
-    //TODO Consider BufReader, though month or two of personal finance fits into memory fine
+    //TODO Could use BufReader, though month or two of personal finance fits into memory fine
     let content = std::fs::read_to_string(&args.infile)
         .with_context(|_| format!("Could not read file '{}'", infilename))?;
     
-    //TODO handle or convert ISO-8859-15, instead of leaving it to the user
+    //TODO MAYBE handle or convert ISO-8859-15, instead of leaving it to the user
     
+    // TODO use this example data in a test
+    let _example = "\"08.06.2020\";\"08.06.2020\";\"Mila Mustermann\";\"Tolle Laden GmbH\";;\"DE89370400440532013000\";;\"GENODEF1HB1\";\"Basislastschrift
+PP.1234.PP . EXAMPLIFIED, I
+hr Einkauf bei EXAMPLIFIED 
+EREF: 1009202807490 PP.1234
+.PP PAYPAL MREF: ABCD123LMN
+DVG CRED: EXAMPLE0000000000
+000000012 IBAN: DE123456789
+98765432103 BIC: DEUTDEFF\";;\"EUR\";\"113,82\";\"S\"";
+
     format(content);
 
     Ok(())
