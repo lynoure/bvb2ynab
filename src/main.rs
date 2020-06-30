b8ec023308a8baaa2127795be15359e63640a0a3
Lynoure Braakman <lynoure@gmail.com>
2020-07-01 00:22:40 +0200

    Optimistic date handling
    YNAB, according to their support is pretty smart with date handling
    so trying it as it is, with only quotes removed.
    
    First steps into TDD in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]  // Locally that would be without the exclamation
+
 use structopt::StructOpt;
 use failure::ResultExt;
 use exitfailure::ExitFailure;
@@ -9,8 +11,17 @@ struct ConversionCLI {
     infile: std::path::PathBuf,
 }
 
+
+// YNAB docs say date format would be "6/8/20" but my account shows "2020-06-08",
+// YNAB support says it autodetects and asks if unclear
+// Leaving the date unconverted for now until hitting a hickup
+fn convert_date(input: String) -> String {
+    input.trim_matches('\"').to_string()
+}
+    
+
 fn format(content: String) {
-    //TODO Error if there this is not found, as it means not BVB bank statement CSV
+    //TODO Error if these are is not found, as it means not BVB bank statement CSV
     // Could also just skip the first 13 lines, but that would be fragile
     let beginning = content.find("\"Buchungstag\";\"Valuta\"").unwrap();
     let transactions = content.get(beginning..).unwrap();
@@ -21,10 +32,12 @@ fn format(content: String) {
         if complete.rfind("\"S\"") == None && complete.rfind("\"H\"") == None {
             continue;
         } else {
+            // Balance lines have very little details and are easy to spot
             if complete.contains(";;;;;;;;;") {
                 break;
             };
             // TODO process the complete line to YNAB format
+            // TODO replace date with the processed one
             println!("{}", complete);
             complete = "".to_string();
         }
@@ -43,6 +56,20 @@ fn main() -> Result<(), ExitFailure> {
     //TODO MAYBE handle or convert ISO-8859-15, instead of leaving it to the user
     
     // TODO use this example data in a test
+    
+    
+    format(content);
+
+    Ok(())
+}
+
+#[test]
+fn test_convert_date() {
+    assert_eq!("28.6.2020".to_string(), convert_date("\"28.6.2020\"".to_string()));
+    assert_eq!("8.6.2020".to_string(), convert_date("\"8.6.2020\"".to_string()));
+}
+
+/*              
     let _example = "\"08.06.2020\";\"08.06.2020\";\"Mila Mustermann\";\"Tolle Laden GmbH\";;\"DE89370400440532013000\";;\"GENODEF1HB1\";\"Basislastschrift
 PP.1234.PP . EXAMPLIFIED, I
 hr Einkauf bei EXAMPLIFIED 
@@ -51,8 +78,4 @@ EREF: 1009202807490 PP.1234
 DVG CRED: EXAMPLE0000000000
 000000012 IBAN: DE123456789
 98765432103 BIC: DEUTDEFF\";;\"EUR\";\"113,82\";\"S\"";
-
-    format(content);
-
-    Ok(())
-}
+*/
